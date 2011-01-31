234a7f5a6b6118bf88533b83f74c25d9d931da5c
Ricardo Cerqueira <cyanogenmod@cerqueira.org>
2011-01-31 00:49:49 +0000

    Squashed commit of breakfast/brunch commands
      To invoke use brunch aosp_grouper to get userdebug by default or
      brunch aosp_grouper user to get a user build
      NOTE that you have to use the the matching lunch entry such as
      full_moto_msm8960 etc
    
    Add a breakfast menu
    
    Make brunch more nutritious
    
    * "breakfast device-name" will set up the environment, just like lunch.
    * "brunch device-name" will actually do the whole setup and build :)
    
    breakfast in bed
    
    breakfast in bed (aliased to "bib") is the baconless friend of lazy
    brunch.
    You can now use "breakfast passion", or "bib passion", to setup the
    corresponding build without actually building it
    
    Fix breakfast for the undecided
    
    The menu presentation (i.e., giving no arguments to breakfast
    or brunch) was broken, so if you didn't know the name of your
    target you weren't given a list to choose from, just an error.
    
    Build: Add support for specifying build variant in brunch/breakfast
    
    This little modification allows specifying build variant in brunch/
    breakfast commands. For example we can use "brunch i9300 user" to build
    user variant instead of default userdebug.
    
    When no extra argument is given, userdebug is default.
    
    Change-Id: I85b8217daa2d511c16a1e82c700e1b0fadbb36ac

--- envsetup.sh
+++ envsetup.sh
@@ -494,6 +494,44 @@ function print_lunch_menu()
     echo
 }
 
+function brunch()
+{
+    breakfast $*
+    if [ $? -eq 0 ]; then
+        mka bacon
+    else
+        echo "No such item in brunch menu. You must specify aosp_DEVICE or full_DEVICE"
+        return 1
+    fi
+    return $?
+}
+
+function breakfast()
+{
+    target=$1
+    local variant=$2
+
+    if [ $# -eq 0 ]; then
+        # No arguments, so let's have the full menu
+        lunch
+    else
+        echo "z$target" | grep -q "-"
+        if [ $? -eq 0 ]; then
+            # A buildtype was specified, assume a full device name
+            lunch $target
+        else
+            # This is probably just the model name
+            if [ -z "$variant" ]; then
+                variant="userdebug"
+            fi
+            lunch $target-$variant
+        fi
+    fi
+    return $?
+}
+
+alias bib=breakfast
+
 function lunch()
 {
     local answer
